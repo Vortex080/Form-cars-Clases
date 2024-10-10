2144c0e016e48ca5479eb99538e0b964c91ddb4b
Antonio Escuchas <antonioescuchas080@gmail.com>
2024-10-10 09:33:31 +0200

    Establecimiento final de archivos y cambio de readme

---
+++ clases/helpers/logIn.php
@@ -0,0 +1,62 @@
+<?php
+
+
+    /**
+     * Inicia la sesión
+    */
+    function iniciaSession(){
+        session_start();
+    }
+
+    /**
+     * Cierra la sesión
+    */
+    function cierraSession() {
+        session_destroy();
+    }
+
+    /**
+     * Crea la session
+     * @var $nombre (para la creacion del $_SESSION)
+    */
+    function logIn($nombre){
+        $_SESSION['user'] = $nombre;
+    }
+
+    /**
+     * Cierra la sesión
+     * @var $user (nombre de la sesión)
+     */
+    function logOut($user) {
+        $_SESSION[$user] = '';
+        cierraSession();
+    }
+
+    /**
+     * Devuelve si la sesión sigue iniciada
+     */
+    function statusLogIn() {
+        return isset($_SESSION['user']);
+    }
+
+    /**
+     * Description
+     * @var $user
+     * @var $valor
+     */
+    function escribirSession($user, $valor){
+        iniciaSession();
+        $_SESSION[$valor] = $user;
+    }
+
+    $host = $_SERVER['HTTP_HOST'];
+    // Ruta del servidor
+    $ruta = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    // Archivo de la ruta
+    $html = 'index.html';
+    logOut($_GET['name']);
+
+    // Redirección al html
+    header("Location:http://$host$ruta/$html");
+
+?>
